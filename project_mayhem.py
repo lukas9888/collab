26bc17066fbe42e1b8983c9de2c0119a426df13e
Chebbbing <118429178+Chebbbing@users.noreply.github.com>
2022-11-22 12:32:41 +0100

    Update project_mayhem.py

--- project_mayhem.py
+++ project_mayhem.py
@@ -1,9 +1,9 @@
+transactions = []
 def transaction_input():
     """The input should be: payer reciver amount
     this for loop is tracing all the transaction and putting them in a list. 
     People who owe units will be given a positive value equal to their debt
     People who are owed units will be given a negative value equal to what they are owed"""
-    transactions = []
     amount_transactions = int(input()) #input on amount of transactions
 
     for x in range(amount_transactions):  
@@ -16,17 +16,30 @@ def transaction_input():
             
             transactions.append([payer, - amount])
             transactions.append([receiver, amount])
-    return transactions
+    
 
 balance = {}
 def settle():
     """This function will settle the debt between the payer and reciver and output the difference """
+    with open('balance.txt', 'r') as balance_text: # This will read if there are any previous transactions made and put it in the dictonary 
+        for users in balance_text:
+            users = users.rstrip()
+            user = users.split(',')
+            if user[0] in balance.keys():
+                balance[user[0]] += int(user[1])
+            else:
+                balance[user[0]] = int(user[1])
+
     for transaction in transactions:
         if transaction[0] not in balance:
             balance.setdefault(transaction[0], transaction[1])
         else:
             total = transaction[1] + balance[transaction[0]]
             balance[transaction[0]] = total
+    with open('balance.txt', 'w') as balance_text: #this will save the transactions made into a dictonary 
+        for users in balance: 
+            balance_text.write(f'{users}, {balance[users]}\n')
+
 
 def view():
     local_balance = balance
@@ -35,10 +48,41 @@ def view():
             if local_balance[payer] > 0:
                 if local_balance[receiver] < 0 and payer != receiver:
                     if -local_balance[receiver] > local_balance[payer]:
-                        print(f"{payer} {receiver} {local_balance[payer]}")
+                        print(f"\n{payer} {receiver} {local_balance[payer]}")
                         local_balance[receiver] = local_balance[receiver] + local_balance[payer]
                         local_balance[payer] = 0
                     else:    
-                        print(f"{payer} {receiver} {-local_balance[receiver]}")
+                        print(f"\n{payer} {receiver} {-local_balance[receiver]}")
                         local_balance[payer] = local_balance[payer] + local_balance[receiver]
                         local_balance[receiver] = 0
+
+
+username_input = ''
+def user_name():
+    users = []
+    with open('users.txt', 'r') as user_database:
+        for usernames in user_database:
+            usernames = usernames.rstrip()
+            users.append(usernames)
+    while True:
+        username_input = input('Username: ') 
+        if username_input in users:
+            username_input = username_input
+            break
+
+
+def new_user_name():
+    existing_users = []
+    with open('users.txt', 'r') as existing_users_text:
+        for user in existing_users_text:
+            user = user.rstrip()
+            existing_users.append(user)
+
+    while True: 
+        new_name = input('New username: ')   
+        if new_name not in existing_users:
+            with open('users.txt', 'a') as user_text:
+                user_text.write('\n' + new_name)
+                break
+        else:
+            print('User allready exists')
