9064f789f665be0fe2385462cd22b18f4acef6bf
Chebbbing <118429178+Chebbbing@users.noreply.github.com>
2022-11-29 19:42:27 +0100

    Update project_mayhem.py

--- project_mayhem.py
+++ project_mayhem.py
@@ -1,14 +1,24 @@
+from project_mayhem import settle, transaction_input, view_user, user_name, view_all, read_balance, write_balance
+
 running = True
 
+read_balance()
+user_name()
 while running:
-    action = input()
-    
+    action = input('Add transaction (i), view (v), log out (l): ')
+
     if action == "q":
         running = False
     
-    elif action == "input":
+    elif action == "i":
         transaction_input()
+        settle()
+        write_balance()
 
 
-    elif action == "view":
-        view()
+    elif action == "v":
+        settle()
+        view_user()
+    
+    elif action == "l":
+        user_name()
