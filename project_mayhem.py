748ff00b7912a3ca51b700b6e8950f2f639a7428
lukas9888 <118428950+lukas9888@users.noreply.github.com>
2022-11-29 18:42:05 +0100

    Update project_mayhem.py

--- project_mayhem.py
+++ project_mayhem.py
@@ -1,44 +1,14 @@
-def transaction_input():
-    """The input should be: payer reciver amount
-    this for loop is tracing all the transaction and putting them in a list. 
-    People who owe units will be given a positive value equal to their debt
-    People who are owed units will be given a negative value equal to what they are owed"""
-    transactions = []
-    amount_transactions = int(input()) #input on amount of transactions
+running = True
 
-    for x in range(amount_transactions):  
-        if amount_transactions >= 1:
-            tosplit = input().split()
+while running:
+    action = input()
+    
+    if action == "q":
+        running = False
+    
+    elif action == "input":
+        transaction_input()
 
-            payer = tosplit[0]
-            receiver = tosplit[1]
-            amount = int(tosplit[2])
-            
-            transactions.append([payer, - amount])
-            transactions.append([receiver, amount])
-    return transactions
 
-balance = {}
-def settle():
-    """This function will settle the debt between the payer and reciver and output the difference """
-    for transaction in transactions:
-        if transaction[0] not in balance:
-            balance.setdefault(transaction[0], transaction[1])
-        else:
-            total = transaction[1] + balance[transaction[0]]
-            balance[transaction[0]] = total
-
-def view():
-    local_balance = balance
-    for payer in local_balance:
-        for receiver in local_balance:
-            if local_balance[payer] > 0:
-                if local_balance[receiver] < 0 and payer != receiver:
-                    if -local_balance[receiver] > local_balance[payer]:
-                        print(f"{payer} {receiver} {local_balance[payer]}")
-                        local_balance[receiver] = local_balance[receiver] + local_balance[payer]
-                        local_balance[payer] = 0
-                    else:    
-                        print(f"{payer} {receiver} {-local_balance[receiver]}")
-                        local_balance[payer] = local_balance[payer] + local_balance[receiver]
-                        local_balance[receiver] = 0
+    elif action == "view":
+        view()
